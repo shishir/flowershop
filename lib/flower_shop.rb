cdb982a1619334254780228073083eef7056ad9e
shishir <shishir.das@gmail.com>
2016-12-26 16:10:26 +1100

    Take input from console

--- lib/flower_shop.rb
+++ lib/flower_shop.rb
@@ -5,6 +5,19 @@ require_relative 'flower_shop/shopping_bag'
 require_relative 'flower_shop/shop'
 
 module FlowerShop
+  def self.handle_customer
+    $/ = "\n\n"
 
+    shop = Shop.new
+    input = gets
 
+    rows = input.split("\n")
+    rows.each do |row|
+     quantity, code = row.split(' ')
+     shop.sell(code, quantity.to_i)
+    end
+    puts shop.bag.to_s
+  end
 end
+
+FlowerShop.handle_customer
